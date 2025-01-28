1b5d34bdf08d3e14625773e2567852dc5fe82f64
hkandaswamy <77952068+hkandaswamy@users.noreply.github.com>
2025-01-28 13:32:54 -0800

    working on states combiner

---
+++ combiner.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("CombineStatesToORC").config('spark.master', 'local[*]').getOrCreate()
+path = "hdfs://localhost:9000/user/test/*.csv"
+
+df = spark.read.csv(path, header=True, inferSchema=True)
+
+orc_path = "hdfs://localhost:9000/user/test/2010_combined_states.orc"
+
+df.write.format('orc').mode('overwrite').save(orc_path)
+
+spark.stop()
+

ab69f134bbffff090bbe08e00024dab44e7ada19
miguelpena-bigdata-dev <miguel674@revature.net>
2025-01-29 07:05:55 -0700

    example method added, and example usage

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,7 +1,17 @@
-class Cleaner():
-    def __init__(self, data):
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col
+
+class DataCleaner():
+
+    def __init__(self, data: DataFrame):
         self.__data = data
+
     @property
-    def data(self):
-        return self.__data
+    def data(self) -> DataFrame:
+        data: DataFrame = self.__data
+        return data
     
+    def test_method(self) -> DataFrame:
+        data: DataFrame = self.__data #doing this allows us to use intellisense to see the various dataframe transformations and actions.
+        data = data[col('SUMLEV') == 50].select(['STUSAB', 'P0010001'])
+        return DataCleaner(data)
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from data_loader import DataLoader
+from data_cleaner import DataCleaner
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col
 from config import HDFS_DATA_DIR_1
 
 def main():
     data_loader = DataLoader(HDFS_DATA_DIR_1)
     data: DataFrame = data_loader.data
 
-    data[col('SUMLEV') == 50].show()
+    cleaned_data: DataFrame = DataCleaner(data) \
+                            .test_method() \
+                            .data
+    
+    cleaned_data.show()
 
     data_loader.stop()
 

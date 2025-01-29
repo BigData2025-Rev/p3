756f4f79f45f68ff43621ed131dbbf8a00d10b05
miguelpena-bigdata-dev <miguel674@revature.net>
2025-01-29 09:43:10 -0700

    removed return type

--- data_cleaner.py
+++ data_cleaner.py
@@ -11,7 +11,7 @@ class DataCleaner():
         data: DataFrame = self.__data
         return data
     
-    def test_method(self) -> DataFrame:
+    def test_method(self):
         data: DataFrame = self.__data #doing this allows us to use intellisense to see the various dataframe transformations and actions.
         data = data[col('SUMLEV') == 50].select(['STUSAB', 'P0010001'])
         return DataCleaner(data)

25a86b494302542d2ec21edd39d3b2e02fddc30b
henryglee <henryglee88@gmail.com>
2025-01-31 11:45:50 -0800

    Add comments to the combine, constant, and main files

--- 2010_code/2010_wrangler_main.py
+++ 2010_code/2010_wrangler_main.py
@@ -1,7 +1,12 @@
+'''
+Main file that reads and processes the census data files for the 2010s. 
+Writes the final dataset in ORC format for efficient storage. 
+'''
+
 from pyspark.sql import SparkSession
 import constant
 
-#create Spark session
+# initialize spark session
 spark = SparkSession.builder\
     .appName("DataProcessing") \
     .config("spark.executor.memory", "4g") \
@@ -11,35 +16,41 @@ spark = SparkSession.builder\
     .config("spark.shuffle.compress", "true") \
     .getOrCreate()
 
+# broadcast constant values and define column mappings GEO_HEADER file
 geo_header_broadcast = spark.sparkContext.broadcast(constant.GEO_HEADER)
+
+# take line from GEO_HEADER file and extract required fields
 def process_line(line):
     geo_header = geo_header_broadcast.value
     return [eval(f"line[{v}]").strip() for k, v in geo_header.items()]
 
+# iterate through list of states and process files for each
 for state in constant.STATES:
-    #grab the 3 files that each state is broken up into
     geo_file = f"final/2010/{state}/*geo2010.pl"
     p1_file = f"final/2010/{state}/*000012010.pl"
     p2_file = f"final/2010/{state}/*000022010.pl"
 
-    #create rdd of the geo file and convert the rdd to a dataframe
+    # read GEO_HEADER file data as an RDD
     geo_rdd = spark.sparkContext.textFile(geo_file)
     geo_rdd = geo_rdd.map(process_line)
     geo_df = geo_rdd.toDF(list(constant.GEO_HEADER.keys()))
 
-    #create dataframe of the part 1 file
+    # load census data
     p1_df = spark.read.csv(p1_file)
     p1_df = p1_df.toDF(*constant.P1_HEADER)
     p1_df = p1_df.drop("FILEID","STUSAB","CHARITER","CIFSN")
 
-    #create dataframe of the part 2 file
     p2_df = spark.read.csv(p2_file)
     p2_df = p2_df.toDF(*constant.P2_HEADER)
     p2_df = p2_df.drop("FILEID","STUSAB","CHARITER","CIFSN")
 
-    #combine the 3 dataframes and write them to file
+    # merge datasets
     output = geo_df.join(p1_df, on="LOGRECNO", how="outer")
     output = output.join(p2_df, on="LOGRECNO", how="outer")
+
+    # sort merged dataset by LOGRECNO
     output = output.sort("LOGRECNO", ascending=True)
     output = output.select(*constant.COMBINE_HEADER)
+
+    # write final merged dataset in ORC format
     output.write.mode("overwrite").orc(f"final/2010/{state}/output")

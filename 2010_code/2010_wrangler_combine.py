6921a9dcfd60520da4fcc59b55fccf661292635b
henryglee <henryglee88@gmail.com>
2025-01-31 11:45:50 -0800

    Add comments to the combine, constant, and main files

--- 2010_code/2010_wrangler_combine.py
+++ 2010_code/2010_wrangler_combine.py
@@ -1,7 +1,12 @@
-from pyspark.sql import SparkSession
-import 2010_wrangler_constant
+'''
+Combine file that processes ORC files for the different U.S. states for the 2010s 
+and combines them into a single DataFrame.
+'''
 
-#create Spark session
+from pyspark.sql import SparkSession
+import 2010_wrangler_constant.py
+
+#initialize spark session
 spark = SparkSession.builder\
     .appName("DataProcessing") \
     .config("spark.executor.memory", "4g") \
@@ -13,20 +18,23 @@ spark = SparkSession.builder\
 
 combine_df = None
 try:
-    #loop through all states in the 2010_wrangler_constant file
+    # load and combine ORC files from different states
     for state in constant.STATES:
         print(f"Combining {state}")
         df = spark.read.orc(f"final/2010/{state}/output")
         print(f"Add {df.count()}")
-        # if combine_df isn't None add df to it
+
+        # merge dataframes
         if combine_df:
             combine_df = combine_df.union(df)
         else:
             combine_df = df
         print(f"Successfully combining {state}")
+
     print(f"TOTAL {combine_df.count()}")
-    #write combine_df to file after looping through all state dataframes
     combine_df.write.mode("overwrite").orc("/final/2010/ALL")
+
+# exception handling
 except Exception as e:
     print(e)
     combine_df.write.mode("overwrite").orc("/final/2010/ALL")

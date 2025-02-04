5baa1b5617c1b6897de9a4b6306a05e1c5365825
miguelpena-bigdata-dev <miguel674@revature.net>
2025-02-04 12:42:49 -0700

    simple linear regression added and obtained results

--- data-analysis/miguel-analysis-app/main.py
+++ data-analysis/miguel-analysis-app/main.py
@@ -1,5 +1,4 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col
 from data_loader import DataLoader
 from national_total_analysis import NationalTotalAnalysis
 from config import HDFS_DATA_DIR
@@ -8,15 +7,24 @@ def main():
 
     data_loader = DataLoader(HDFS_DATA_DIR)
     clean_data: DataFrame = data_loader.data
-    clean_data.printSchema()
+    # clean_data.printSchema()
 
     result: DataFrame = NationalTotalAnalysis(clean_data) \
                         .get_state_level_data() \
                         .aggregate_population_by_year() \
                         .data
     
+    future_result: DataFrame = NationalTotalAnalysis(clean_data) \
+                        .get_state_level_data() \
+                        .aggregate_population_by_year() \
+                        .using_future_population() \
+                        .add_year(2030) \
+                        .data
+    # NationalTotalAnalysis(clean_data) \
+    #                     .get_trend_line()
+    result = result.union(future_result.select(['year', 'total_population']))
     result.show()
-    result.printSchema()
+    # result.printSchema()
     data_loader.stop()
 
 if __name__ == '__main__':
--- data-analysis/miguel-analysis-app/national_total_analysis.py
+++ data-analysis/miguel-analysis-app/national_total_analysis.py
@@ -1,5 +1,7 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import col, pandas_udf, lit
+import pandas as pd
 
 
 class NationalTotalAnalysis():
@@ -15,11 +17,45 @@ class NationalTotalAnalysis():
         data = data.filter(col('summary_level') == 40)
         return NationalTotalAnalysis(data)
     
+    @staticmethod
+    @pandas_udf(returnType=IntegerType())
+    def make_prediction(independent: pd.Series, dependent: pd.Series) -> int:
+        """
+            Params: Independent and Dependent Column
+            Returns: Predicted value assuming linear relationship.
+        """
+        TARGET_YEAR = 2030
+        n = independent.count()
+
+        x_sum = independent.sum()
+        y_sum = dependent.sum()
+        x_squared_sum = (independent * independent).sum()
+        xy_sum = (independent * dependent).sum()
+
+        intercept = (y_sum*x_squared_sum - x_sum * xy_sum) / (n * x_squared_sum - (x_sum ** 2))
+        slope = (n * xy_sum - x_sum * y_sum) / (n * x_squared_sum - (x_sum ** 2))
+        prediction = intercept + slope * TARGET_YEAR
+
+        return round(prediction)
+                
+    def using_future_population(self):
+        data: DataFrame = self.__data
+        data = data.withColumn('id', lit(1))
+        data = data.groupBy('id').agg(self.make_prediction(col('year'), col('total_population')).alias('total_population'))
+        return NationalTotalAnalysis(data)
+    
+    def add_year(self, year: int):
+        data: DataFrame = self.__data
+        data = data.withColumn('year', lit(year)) \
+                    .drop('id')
+        return NationalTotalAnalysis(data)
+    
     def aggregate_population_by_year(self):
         data: DataFrame = self.__data
         data = data.groupBy('year') \
                     .agg({'total_population':'sum'}) \
-                    .withColumnRenamed('sum(total_population)', 'total_population')
+                    .withColumnRenamed('sum(total_population)', 'total_population') \
+                    .sort('year')
         return NationalTotalAnalysis(data)
 
     

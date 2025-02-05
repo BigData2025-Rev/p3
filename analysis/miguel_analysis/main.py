314e479f014bdfe6cb0d1fe387e5df7125e38d77
miguelpena-bigdata-dev <miguel674@revature.net>
2025-02-05 12:18:57 -0700

    added percent change column, saved result for PowerBI import

--- analysis/miguel_analysis/main.py
+++ analysis/miguel_analysis/main.py
@@ -1,7 +1,21 @@
+import os
+from pathlib import Path
 from pyspark.sql import DataFrame
 from data_loader import DataLoader
 from national_total_analysis import NationalTotalAnalysis
 from config import HDFS_DATA_DIR
+
+
+def save_dataframe_to_localpath(data: DataFrame, filename: str):
+    #get absolute path to current working directory
+    path = Path(__file__).parent / filename
+
+    #reduce partitions to one
+    data: DataFrame = data.coalesce(1)
+
+    #save to orc file
+    data.write.mode('overwrite').format('orc').save(f"file://{str(path.absolute())}")
+
 
 def main():
 
@@ -14,17 +28,28 @@ def main():
                         .aggregate_population_by_year() \
                         .data
     
+    
+    
     future_result: DataFrame = NationalTotalAnalysis(clean_data) \
                         .get_state_level_data() \
                         .aggregate_population_by_year() \
                         .using_future_population() \
                         .add_year(2030) \
                         .data
+    
+    
     # NationalTotalAnalysis(clean_data) \
     #                     .get_trend_line()
     result = result.union(future_result.select(['year', 'total_population']))
-    result.show()
-    result.printSchema()
+    percent_change: DataFrame = NationalTotalAnalysis(result) \
+                        .using_percent_growth() \
+                        .data
+    # result.show()
+    percent_change.show()
+
+    save_dataframe_to_localpath(percent_change, 'analysis_result.orc')
+
+    # result.printSchema()
     data_loader.stop()
 
 if __name__ == '__main__':
--- analysis/miguel_analysis/national_total_analysis.py
+++ analysis/miguel_analysis/national_total_analysis.py
@@ -1,6 +1,7 @@
 from pyspark.sql import DataFrame
 from pyspark.sql.types import IntegerType
-from pyspark.sql.functions import col, pandas_udf, lit
+from pyspark.sql.functions import col, pandas_udf, lit, lag, when, bround
+from pyspark.sql.window import Window
 import pandas as pd
 
 
@@ -41,13 +42,19 @@ class NationalTotalAnalysis():
     def using_future_population(self):
         data: DataFrame = self.__data
         data = data.withColumn('id', lit(1))
-        data = data.groupBy('id').agg(self.make_prediction(col('year'), col('total_population')).alias('total_population'))
+        data = data.groupBy('id') \
+                    .agg(self.make_prediction(col('year'), col('total_population'))) \
+                    .withColumnRenamed('make_prediction(year, total_population)','total_population')
+        
         return NationalTotalAnalysis(data)
     
     def add_year(self, year: int):
         data: DataFrame = self.__data
-        data = data.withColumn('year', lit(year)) \
-                    .drop('id')
+        data = data.withColumn('year', lit(year))
+
+        if 'id' in data.columns:
+            data = data.drop('id')
+        
         return NationalTotalAnalysis(data)
     
     def aggregate_population_by_year(self):
@@ -56,6 +63,21 @@ class NationalTotalAnalysis():
                     .agg({'total_population':'sum'}) \
                     .withColumnRenamed('sum(total_population)', 'total_population') \
                     .sort('year')
+        
+        return NationalTotalAnalysis(data)
+    
+    def using_percent_growth(self):
+        data: DataFrame = self.__data
+        population_window = Window.orderBy('year')
+        data = data.withColumn('prev_population', lag('total_population').over(population_window)) \
+                    .withColumn('percent_growth',  
+                                when(col('prev_population').isNotNull(),
+                                     (col('total_population') - col('prev_population')) / col('prev_population') * 100) \
+                                .otherwise(None)) \
+                    .withColumn('percent_growth', when(col('percent_growth').isNotNull(),
+                                                       bround('percent_growth', 2)) \
+                                                    .otherwise(None))
+        
         return NationalTotalAnalysis(data)
 
     

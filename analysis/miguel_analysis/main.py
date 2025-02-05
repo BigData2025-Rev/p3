c0c338917a5eab6c2b602d33c6c005678e58b9bb
miguelpena-bigdata-dev <miguel674@revature.net>
2025-02-05 12:29:01 -0700

    removed commented out code

--- analysis/miguel_analysis/main.py
+++ analysis/miguel_analysis/main.py
@@ -21,7 +21,6 @@ def main():
 
     data_loader = DataLoader(HDFS_DATA_DIR)
     clean_data: DataFrame = data_loader.data
-    # clean_data.printSchema()
 
     result: DataFrame = NationalTotalAnalysis(clean_data) \
                         .get_state_level_data() \
@@ -37,19 +36,13 @@ def main():
                         .add_year(2030) \
                         .data
     
-    
-    # NationalTotalAnalysis(clean_data) \
-    #                     .get_trend_line()
     result = result.union(future_result.select(['year', 'total_population']))
     percent_change: DataFrame = NationalTotalAnalysis(result) \
                         .using_percent_growth() \
                         .data
-    # result.show()
     percent_change.show()
 
     save_dataframe_to_localpath(percent_change, 'analysis_result.orc')
-
-    # result.printSchema()
     data_loader.stop()
 
 if __name__ == '__main__':
